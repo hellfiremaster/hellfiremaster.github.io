907c74e60a0c54a4fe3306b8d8759f6c5b516b01
hellfiremaster <helloyongsa@gmail.com>
2025-04-19 00:11:44 +0900

    sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+//서비스 워커는 importScripts로만 로드해용
+importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-etI5o4uWbBp2Arj9KpxdDyRebguErnY",
+  authDomain: "newpro-4bc73.firebaseapp.com",
+  projectId: "newpro-4bc73",
+  storageBucket: "newpro-4bc73.firebasestorage.app",
+  messagingSenderId: "267192611563",
+  appId: "1:267192611563:web:94fb5ec97872cd452a84ae"
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  console.log('Message received. ', payload);  
+  self.registration.showNotification(
+    payload.notification.title,
+    payload.notification
+  );
+});
+
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+});
