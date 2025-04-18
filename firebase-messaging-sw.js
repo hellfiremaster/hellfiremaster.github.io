c550f5321ed869b70ad21111c1f0edea14c9d9c2
hellfiremaster <helloyongsa@gmail.com>
2025-04-19 00:33:57 +0900

    노티 테스트

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -21,6 +21,25 @@ messaging.onBackgroundMessage(payload => {
     // );
   });
   
-  messaging.onMessage((payload) => {
+messaging.onMessage((payload) => {
     console.log('onMessage received. ', payload);
+});
+
+// 백그라운드 메시지는 자동으로 노티로 보여준다고 가정
+// 클릭 시 동작 정의
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    const { postId, commentId } = event.notification.data || {};
+    const url = `${self.location.origin}/?postId=${postId}&commentId=${commentId}`;
+    event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true })
+        .then(winList => {
+          // 이미 열린 창 있으면 포커스
+          for (const win of winList) {
+            if (win.url === url) return win.focus();
+          }
+          // 없으면 새창
+          return clients.openWindow(url);
+        })
+    );
   });
