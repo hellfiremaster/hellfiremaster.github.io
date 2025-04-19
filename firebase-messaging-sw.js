cd179977065d42b5ce204cbaa0b71db411abe003
hellfiremaster <helloyongsa@gmail.com>
2025-04-19 09:48:37 +0900

    fcm test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,33 +14,29 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-    console.log('onBackgroundMessage received.', payload);  
-    // self.registration.showNotification(
-    //   payload.notification.title,
-    //   payload.notification
-    // );
-  });
-  
-messaging.onMessage((payload) => {
-    console.log('onMessage received. ', payload);
+  console.log('onBackgroundMessage received.', payload);  
+  // self.registration.showNotification(
+  //   payload.notification.title,
+  //   payload.notification
+  // );
 });
 
 // 백그라운드 메시지는 자동으로 노티로 보여준다고 가정
 // 클릭 시 동작 정의
 self.addEventListener('notificationclick', event => {
     console.log('notificationclick', event);
-    event.notification.close();
-    const { postId, commentId } = event.notification.data || {};
-    const url = `${self.location.origin}/?postId=${postId}&commentId=${commentId}`;
-    event.waitUntil(
-      clients.matchAll({ type: 'window', includeUncontrolled: true })
-        .then(winList => {
-          // 이미 열린 창 있으면 포커스
-          for (const win of winList) {
-            if (win.url === url) return win.focus();
-          }
-          // 없으면 새창
-          return clients.openWindow(url);
-        })
-    );
+//     event.notification.close();
+//     const { postId, commentId } = event.notification.data || {};
+//     const url = `${self.location.origin}/?postId=${postId}&commentId=${commentId}`;
+//     event.waitUntil(
+//       clients.matchAll({ type: 'window', includeUncontrolled: true })
+//         .then(winList => {
+//           // 이미 열린 창 있으면 포커스
+//           for (const win of winList) {
+//             if (win.url === url) return win.focus();
+//           }
+//           // 없으면 새창
+//           return clients.openWindow(url);
+//         })
+//     );
   });
