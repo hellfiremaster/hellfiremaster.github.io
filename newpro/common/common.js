ceb869701b70c8ee0becfef9c6494bd7fead6ade
hellmaster <helloyongsa@gmail.com>
2025-08-16 11:42:15 +0900

    경로?

--- newpro/common/common.js
+++ newpro/common/common.js
@@ -181,17 +181,21 @@ async function loadPart(containerSelector, filePath) {
 }
 
 function updateActiveMenu() {
-  // 현재 페이지의 파일 이름만 가져오기 (예: "index.html" 또는 "card.html")
-  const currentPage = window.location.pathname.split('/').pop();
+  // 현재 페이지의 파일 이름만 가져오기
+  let currentPage = window.location.pathname.split('/').pop();
 
-  // #mainNav 안의 모든 nav-link를 선택
+  // 파일명이 없으면 index.html로 처리
+  if (!currentPage) {
+    currentPage = 'index.html';
+  }
+
   const navLinks = document.querySelectorAll('#mainNav .nav-link');
 
   navLinks.forEach(link => {
-    // 각 링크의 href 속성에서 파일 이름만 가져오기
-    const linkPage = link.getAttribute('href').split('/').pop();
-
-    // 현재 페이지와 링크의 페이지가 같으면 'active' 클래스 추가!
+    let linkPage = link.getAttribute('href').split('/').pop();
+    if (!linkPage) {
+      linkPage = 'index.html';
+    }
     if (linkPage === currentPage) {
       link.classList.add('active');
     }
